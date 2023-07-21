59e56272e46b41046ebf41df5cd4d05cdd0f1363
Sergey Zalivshiy <segga17@gmail.com>
2023-07-21 11:13:07 +0300

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+		"prefer-const": "error",
+		"semi": ["warn", "always"],
+		"react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }]
+	  }
+}
